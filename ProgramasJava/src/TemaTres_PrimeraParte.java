862b4b17389f1608e0128f074e448a4d18ba4012
Diana <diana1310fg@gmail.com>
2023-02-02 21:22:32 -0600

    Suma de tres números.

---
+++ ProgramasJava/src/TemaTres_PrimeraParte.java
@@ -0,0 +1,12 @@
+public class TemaTres_PrimeraParte {
+
+    public static void main(String[] args) {
+        int resultado = suma(10, 20, 30);
+        System.out.println("resultado = " + resultado);
+    }
+
+    private static int suma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+}
