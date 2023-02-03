9968004bfb84f18c699745b162644d689f63c243
Diana <diana1310fg@gmail.com>
2023-02-02 21:23:02 -0600

    Uso de clases y objetos.

--- ProgramasJava/src/TemaTres_SegundaParte.java
+++ ProgramasJava/src/TemaTres_SegundaParte.java
@@ -1,5 +1,17 @@
 public class TemaTres_SegundaParte {
 
-    
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.incrementaPuertas();
+        System.out.println(miCoche.numeroPuertas);
+    }
 
+}
+
+class Coche {
+    int numeroPuertas = 4;
+
+    void incrementaPuertas () {
+        numeroPuertas++;
+    }
 }
