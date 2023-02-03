efb3fbe8f77cb6295d6004d4df60da8f917f3e1a
Diana <diana1310fg@gmail.com>
2023-02-02 21:29:29 -0600

    Ejercicio Tema tres.

--- ProgramasJava/src/TemaTres.java
+++ ProgramasJava/src/TemaTres.java
@@ -1,9 +1,16 @@
-public class TemaTres_SegundaParte {
+public class TemaTres {
 
     public static void main(String[] args) {
+        int resultado = suma(10, 20, 30);
+        System.out.println("El resultado de la suma es: " + resultado);
+
         Coche miCoche = new Coche();
         miCoche.incrementaPuertas();
         System.out.println("El coche tiene " + miCoche.numeroPuertas + " puertas.");
+    }
+
+    private static int suma(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
     }
 
 }
