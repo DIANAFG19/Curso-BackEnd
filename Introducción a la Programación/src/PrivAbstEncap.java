5d8606c0c7408a25be22a4e198432d8722e50494
Diana <diana1310fg@gmail.com>
2023-02-16 21:11:19 -0600

    Tema 8: Inicio de Clases Abstractas.

--- Introducción a la Programación/src/PrivAbstEncap.java
+++ Introducción a la Programación/src/PrivAbstEncap.java
@@ -1,5 +1,6 @@
 public class PrivAbstEncap {
     public static void main(String[] args) {
+        /*
         Vehiculo coche = new Vehiculo();
         coche.setTipo("Jetta");
         coche.setVelocidadMaxima(220);
@@ -10,10 +11,11 @@ public class PrivAbstEncap {
 
         System.out.println("El coche " + coche.getTipo() + " tiene una velocidad máxima de " + coche.getVelocidadMaxima() + " km/h");
         System.out.println("La moto " + moto.getTipo() + " tiene una velocidad máxima de " + moto.getVelocidadMaxima() + " km/h");
+         */
     }
 }
 
-class Vehiculo {
+abstract class Vehiculo {
     private String tipo;
     private int velocidadMaxima;
 
