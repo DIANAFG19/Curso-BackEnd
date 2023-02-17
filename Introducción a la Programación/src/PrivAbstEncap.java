3172550d37cb779de2ff278f700ae93c8772f788
Diana <diana1310fg@gmail.com>
2023-02-16 20:13:36 -0600

    Tema 8: Encapsulación, Setter & Getter.

--- Introducción a la Programación/src/PrivAbstEncap.java
+++ Introducción a la Programación/src/PrivAbstEncap.java
@@ -2,23 +2,34 @@ public class PrivAbstEncap {
     public static void main(String[] args) {
         Vehiculo coche = new Vehiculo();
         coche.setTipo("Jetta");
+        coche.setVelocidadMaxima(220);
 
         Vehiculo moto = new Vehiculo();
         moto.setTipo("Italika");
+        moto.setVelocidadMaxima(120);
 
-        System.out.println(coche.getTipo());
-        System.out.println(moto.getTipo());
-
+        System.out.println("El coche " + coche.getTipo() + " tiene una velocidad máxima de " + coche.getVelocidadMaxima() + " km/h");
+        System.out.println("La moto " + moto.getTipo() + " tiene una velocidad máxima de " + moto.getVelocidadMaxima() + " km/h");
     }
 }
 
 class Vehiculo {
     private String tipo;
+    private int velocidadMaxima;
 
-    public void setTipo(String valor) {
-        this.tipo = valor;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
     public String getTipo() {
         return this.tipo;
     }
+
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+
 }
