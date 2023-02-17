90315caf0cb94a22c76c028f5f9152a1b81d532b
Diana <diana1310fg@gmail.com>
2023-02-16 21:20:45 -0600

    EJERCICIO TEMA 8: Encapsulación.

---
+++ Introducción a la Programación/src/TemaOcho.java
@@ -0,0 +1,39 @@
+public class TemaOcho {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Diana");
+        persona.setEdad(26);
+        persona.setTelefono("5512345678");
+
+        System.out.println("El nombre es " + persona.getNombre() + ", tiene " + persona.getEdad() +
+                " años y su número de teléfono es " + persona.getTelefono() + ".");
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+
+}
