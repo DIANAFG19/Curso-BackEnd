8b15d923ab1fa899c60e2c02c9284b0f65d3a12e
Diana <diana1310fg@gmail.com>
2023-02-16 20:08:31 -0600

    Tema 8: Encapsulación, Setter & Getter.

--- Introducción a la Programación/src/PrivAbstEncap.java
+++ Introducción a la Programación/src/PrivAbstEncap.java
@@ -1,9 +1,24 @@
 public class PrivAbstEncap {
     public static void main(String[] args) {
-        
+        Vehiculo coche = new Vehiculo();
+        coche.setTipo("Jetta");
+
+        Vehiculo moto = new Vehiculo();
+        moto.setTipo("Italika");
+
+        System.out.println(coche.getTipo());
+        System.out.println(moto.getTipo());
+
     }
 }
 
 class Vehiculo {
     private String tipo;
+
+    public void setTipo(String valor) {
+        this.tipo = valor;
+    }
+    public String getTipo() {
+        return this.tipo;
+    }
 }
